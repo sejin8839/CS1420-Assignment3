4513856e415e679d425c95928da884e0cc2a369e
sejin8839 <sejinon01@gmail.com>
2023-03-29 01:52:32 +0900

    Create Assignment3_P2.cpp

---
+++ Assignment3_P2.cpp
@@ -0,0 +1,39 @@
+/* 
+  Author:          
+  Date:
+  Program: Armstrong numbers between 1 and 1000.
+  filename: Assignment3_P2.cpp
+*/
+ 
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+//function prototypes
+int sumCube(int);
+void ArmstrongNumber();
+
+int main()
+{
+    cout<<"Armstrong numbers between 1 and 1000 : ";
+    // Student --- TODOS
+
+}
+/********************************************************
+ * Definition of :  int sumCube(int num)                *
+ * This function calculate sum of cubes of its digits   *
+ * and returns the result                               *
+ ********************************************************/
+int sumCube(int num)
+{
+    // Student --- TODOS
+}
+/********************************************************
+ * Definition of :  void ArmstrongNumber()              *
+ * This function identifies armsrong number between     *
+ * 1 and 1000.                                          *
+ ********************************************************/
+void ArmstrongNumber()
+{
+   // Student --- TODOS
+}
